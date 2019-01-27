7b9793e31d3876719f526c23d2c11b9eb04e4de1
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-01-27 21:07:47 +0100

    eee

--- auth.py
+++ auth.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask
 from flask_restful import Api
 from datetime import timedelta
+from db import db
+from flask_restful import Resource
 
 class Auth(Resource):
     def get(self):
-
         return {"Username":"WaitingAdmin",
                 "Password":"WaitingPassword"}
