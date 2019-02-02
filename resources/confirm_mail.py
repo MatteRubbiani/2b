0c065f4bc04c228776f0d4370a073d137a8f24f0
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-02-02 22:38:53 +0100

    wwww

--- resources/confirm_mail.py
+++ resources/confirm_mail.py
@@ -7,16 +7,17 @@ from models.users import UserModel
 
 class ConfirmMail (Resource):
     def get (self, token):
+
         s = URLSafeTimedSerializer("password1")
         try:
             mail=s.loads(token, salt="emailconfirm")
             user=UserModel.find_by_mail(mail)
             if user:
-                if user.confirmed==False:
-                    user.confirmed=True
+                if user.confermato==False:
+                    user.confermato=True
                     user.save_to_db()
                     return "user confirmed"
-                return "user already confirmed"
+                return "user already confirmed", 400 
             return "user does not exist", 400
         except:
             return "your token is expired"
